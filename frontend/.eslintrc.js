0b09d1297fb539b68193e22f936537cc57860327
dirdr <adrien.pelfresne@gmail.com>
2024-04-21 11:24:14 +0200

    fix(formatting) format buffers

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,31 +2,31 @@ export default {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'standard-with-typescript',
     'plugin:react/recommended',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {}
+  rules: {},
 };
--- frontend/lib/callAPI.ts
+++ frontend/lib/callAPI.ts
@@ -1,20 +1,20 @@
 export const getVideoFromText = async (text: string, spoken: string = 'fr', signed: string = 'fsl') => {
-	const baseURL: string = 'https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose'
-	const fullURL: string = `${baseURL}?text=${text}&spoken=${spoken}&signed=${signed}`
-	const response: Response = await fetch(fullURL)
-	if (!response.ok) return
-	if (response.body == null) return
-	const arrayBuffer: ArrayBuffer = await response.arrayBuffer()
-	const byteArray: Uint8Array = new Uint8Array(arrayBuffer)
+  const baseURL: string = 'https://us-central1-sign-mt.cloudfunctions.net/spoken_text_to_signed_pose';
+  const fullURL: string = `${baseURL}?text=${text}&spoken=${spoken}&signed=${signed}`;
+  const response: Response = await fetch(fullURL);
+  if (!response.ok) return;
+  if (response.body == null) return;
+  const arrayBuffer: ArrayBuffer = await response.arrayBuffer();
+  const byteArray: Uint8Array = new Uint8Array(arrayBuffer);
 
-	// TODO a mettre dans une variable d'environnement (l'url du backend).!!
+  // TODO a mettre dans une variable d'environnement (l'url du backend).!!
   const responseVideo = await fetch('https://signalingo.xyz/backend/pose-to-video', {
     method: 'POST',
-		mode: 'cors',
-		body: JSON.stringify({ data : byteArray.join() })
-  })
-	const blob = await responseVideo.blob();
-	const url = URL.createObjectURL(blob);
-	return url
-}
+    mode: 'cors',
+    body: JSON.stringify({ data: byteArray.join() }),
+  });
 
+  const blob = await responseVideo.blob();
+  const url = URL.createObjectURL(blob);
+  return url;
+};
