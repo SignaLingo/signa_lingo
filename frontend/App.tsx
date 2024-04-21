68d6baf199679b0a1571566bec806314f2fb1087
dirdr <adrien.pelfresne@gmail.com>
2024-04-21 10:51:23 +0200

    fix(frontend) formatting

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,25 +1,31 @@
-import React, { useState } from 'react';
-import { Alert, StyleSheet, View, Pressable, Text, TextInput } from 'react-native';
-import { Audio, ResizeMode, Video } from 'expo-av';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { getVideoFromText } from './lib/callAPI';
-import * as DocumentPicker from 'expo-document-picker';
-import { client } from '@gradio/client';
-import { MaterialIcons } from '@expo/vector-icons';
-
+import React, { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  View,
+  Pressable,
+  Text,
+  TextInput,
+} from "react-native";
+import { Audio, ResizeMode, Video } from "expo-av";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { getVideoFromText } from "./lib/callAPI";
+import * as DocumentPicker from "expo-document-picker";
+import { client } from "@gradio/client";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
   const [recording, setRecording] = useState<Audio.Recording | undefined>();
   const [lastRec, setLastRec] = useState<any>();
-  const [audioFile, setAudioFile] = useState({ name: '', data: '' });
-  const [dataOutput, setDataOutput] = useState('Data');
-  const [statusOutput, setStatusOutput] = useState('Status');
+  const [audioFile, setAudioFile] = useState({ name: "", data: "" });
+  const [dataOutput, setDataOutput] = useState("Data");
+  const [statusOutput, setStatusOutput] = useState("Status");
   const spaceName = "tlemagny/signalingo_whisper";
   const readingToken = "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";
   const writingToken = "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";
   const [isTranscribing, setIsTranscribing] = useState(false);
   const blobToBase64 = (blob: Blob): Promise<string> => {
-  	return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onerror = reject;
       reader.onloadend = () => {
@@ -29,13 +35,11 @@ export default function App() {
     });
   };
 
-  async function transcribe(){
-    if(!audioFile.name) {
-      Alert.alert(
-        "No file given",
-        "No audio file given as input.",
-        [{ text: "OK" }]
-      );
+  async function transcribe() {
+    if (!audioFile.name) {
+      Alert.alert("No file given", "No audio file given as input.", [
+        { text: "OK" },
+      ]);
       console.log("No audio file given as input.");
       return;
     }
@@ -43,7 +47,7 @@ export default function App() {
       Alert.alert(
         "Transcription in Progress",
         "Transcription is already in progress. Please wait.",
-        [{ text: "OK" }]
+        [{ text: "OK" }],
       );
       console.log("Transcription is already in progress. Please wait.");
       return;
@@ -52,45 +56,52 @@ export default function App() {
     //const app = await client(spaceName, {hf_token : readingToken});
     //const app = await client("openai/whisper");
     const app = await client("hf-audio/whisper-large-v3", {});
-    await updateHardware(writingToken,spaceName,"cpu-basic");
+    await updateHardware(writingToken, spaceName, "cpu-basic");
     const submission = app.submit("/predict_1", [audioFile, "transcribe"]);
     setIsTranscribing(true);
     submission.on("data", (data) => {
-        console.log(data);
-        setDataOutput(data.data[0] as string);
-        setIsTranscribing(false);
-		getVideoFromText(data.data[0] as string).then(url => setVideoURL(url))
-        updateHardware(writingToken,spaceName,"cpu-basic");
+      console.log(data);
+      setDataOutput(data.data[0] as string);
+      setIsTranscribing(false);
+      getVideoFromText(data.data[0] as string).then((url) => setVideoURL(url));
+      updateHardware(writingToken, spaceName, "cpu-basic");
     });
     submission.on("status", (status) => {
-        console.log(status);
-        setStatusOutput(`Status: ${status.stage}, Queue Position: ${status.position}, Queue Size: ${status.size}`);
+      console.log(status);
+      setStatusOutput(
+        `Status: ${status.stage}, Queue Position: ${status.position}, Queue Size: ${status.size}`,
+      );
     });
   }
 
-  async function updateHardware(hfToken: String, spaceName: String, hardwareFlavor: String, sleepTimeSeconds = 3600) {
+  async function updateHardware(
+    hfToken: String,
+    spaceName: String,
+    hardwareFlavor: String,
+    sleepTimeSeconds = 3600,
+  ) {
     const url = `https://huggingface.co/api/spaces/${spaceName}/hardware`;
     const headers = {
-        "Authorization": `Bearer ${hfToken}`,
-        "Content-Type": "application/json"
+      Authorization: `Bearer ${hfToken}`,
+      "Content-Type": "application/json",
     };
     const body = JSON.stringify({
-        "flavor": hardwareFlavor,
-        "sleepTimeSeconds": sleepTimeSeconds
+      flavor: hardwareFlavor,
+      sleepTimeSeconds: sleepTimeSeconds,
     });
 
     const response = await fetch(url, {
-        method: "POST",
-        headers: headers,
-        body: body
+      method: "POST",
+      headers: headers,
+      body: body,
     });
     if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
     console.log("Hardware update response:", data);
     return data;
-}
+  }
 
   async function startRecording() {
     try {
@@ -98,9 +109,11 @@ export default function App() {
       if (perm.status === "granted") {
         await Audio.setAudioModeAsync({
           allowsRecordingIOS: true,
-          playsInSilentModeIOS: true
+          playsInSilentModeIOS: true,
         });
-        const { recording: newRecording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
+        const { recording: newRecording } = await Audio.Recording.createAsync(
+          Audio.RecordingOptionsPresets.HIGH_QUALITY,
+        );
         setRecording(newRecording);
       }
     } catch (err) {}
@@ -108,17 +121,17 @@ export default function App() {
 
   async function uriToBase64(uri: string): Promise<string> {
     try {
-    // Use fetch to get the response as a blob
+      // Use fetch to get the response as a blob
       const response = await fetch(uri);
       if (!response.ok) {
-        throw new TypeError('Network request failed');
+        throw new TypeError("Network request failed");
       }
       const blob = await response.blob();
 
       // Convert blob to Base64
       return await blobToBase64(blob);
     } catch (error) {
-      console.error('Error converting URI to Base64:', error);
+      console.error("Error converting URI to Base64:", error);
       throw error; // Re-throw to handle this error further up in your application
     }
   }
@@ -139,64 +152,63 @@ export default function App() {
 
   async function pickDocument() {
     let result = await DocumentPicker.getDocumentAsync({});
-    const file = result.assets?.[0] as DocumentPicker.DocumentPickerAsset
+    const file = result.assets?.[0] as DocumentPicker.DocumentPickerAsset;
     setAudioFile({
       name: file.name,
       data: file.uri.split(",")[1],
     });
-  };
+  }
 
   function displayMainContent() {
     if (recording) {
-      return (
-        <Text style={styles.text}>
-          Écoute...
-        </Text>
-      );
-    }
-    else if (lastRec) {
-      return (
-        <img style={styles.sl} src="./assets/slPlaceholder.png"/>
-      );
-    }
-    else {
-      return (
-        <Text style={styles.text}>
-          Appuyer pour commencer
-        </Text>
-      );
+      return <Text style={styles.text}>Écoute...</Text>;
+    } else if (lastRec) {
+      return <img style={styles.sl} src="./assets/slPlaceholder.png" />;
+    } else {
+      return <Text style={styles.text}>Appuyer pour commencer</Text>;
     }
   }
 
-
-  const [videoURL, setVideoURL] = useState<string | null>()
-  const video = React.useRef(null);	
+  const [videoURL, setVideoURL] = useState<string | null>();
+  const video = React.useRef(null);
   return (
     <View style={styles.container}>
       <View style={styles.mainContainer}>
-    	<Video
-			style={styles.video}
-			ref={video}
-			useNativeControls
-			source={{
-				uri: videoURL as string
-			}}
-			resizeMode={ResizeMode.CONTAIN}
-			videoStyle={{'position': 'relative'}}
-		/>
+        <Video
+          style={styles.video}
+          ref={video}
+          useNativeControls
+          source={{
+            uri: videoURL as string,
+          }}
+          resizeMode={ResizeMode.CONTAIN}
+          videoStyle={{ position: "relative" }}
+        />
         <Text style={styles.text}>Status: {statusOutput}</Text>
         <Text style={styles.text}>Data: {dataOutput}</Text>
       </View>
       <View style={styles.bottomContainer}>
-        <Pressable style={[styles.pressable, { backgroundColor: recording ? '#ff3333' : '#444950' }]} onPress={recording ? stopRecording : startRecording}>
+        <Pressable
+          style={[
+            styles.pressable,
+            { backgroundColor: recording ? "#ff3333" : "#444950" },
+          ]}
+          onPress={recording ? stopRecording : startRecording}
+        >
           <Icon name="microphone" style={styles.icon} />
         </Pressable>
-        <Pressable style={[styles.pressable, { backgroundColor: '#444950'}]} onPress={transcribe}>
+        <Pressable
+          style={[styles.pressable, { backgroundColor: "#444950" }]}
+          onPress={transcribe}
+        >
           <MaterialIcons name="transcribe" style={styles.icon} />
         </Pressable>
-        <Pressable style={[styles.pressable, { backgroundColor: '#444950'}]} onPress={pickDocument}>
+        <Pressable
+          style={[styles.pressable, { backgroundColor: "#444950" }]}
+          onPress={pickDocument}
+        >
           <Icon name="upload" style={styles.icon} />
-      	</Pressable>
+        </Pressable>
       </View>
     </View>
   );
@@ -205,51 +217,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#20232a',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#20232a",
+    alignItems: "center",
+    justifyContent: "center",
   },
   mainContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     flexGrow: 8,
   },
   bottomContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     flexGrow: 2,
   },
   text: {
-    color: 'white',
-    fontFamily: 'system-ui,-apple-system,sans-serif',
+    color: "white",
+    fontFamily: "system-ui,-apple-system,sans-serif",
     fontSize: 18,
   },
   input: {
-  	backgroundColor: 'white'
+    backgroundColor: "white",
   },
   video: {
-	backgroundColor: 'white',
-	alignSelf: 'center',
-	width: 320,
-	height: 320
+    backgroundColor: "white",
+    alignSelf: "center",
+    width: 320,
+    height: 320,
   },
   pressable: {
     borderRadius: 40,
     width: 70,
     height: 70,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginLeft: 10,
     marginRight: 10,
   },
   icon: {
-    color: 'white',
+    color: "white",
     fontSize: 30,
   },
   sl: {
-    width: '90%',
-  }
+    width: "90%",
+  },
 });
