c71a0f566a6025aa7f60b779319414f5e74a585a
AlexVplle <alexisvapaille11@gmail.com>
2024-06-06 02:02:48 +0200

    mobile responsive

--- frontend/components/TextToPose.tsx
+++ frontend/components/TextToPose.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet, Dimensions } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import InputBox from './InputBox';
 import { AntDesign } from '@expo/vector-icons';
 import PoseView from './PoseView';
@@ -8,11 +8,24 @@ const TextToPose = () => {
   const [toTranslate, setToTranslate] = useState<string | undefined>(undefined);
   const [languageCode, setLanguageCode] = useState<string>('fr');
   const [signLanguageCode, setSignLanguageCode] = useState<string>('fsl');
+  const [flexDirection, setFlexDirection] = useState<'row' | 'column'>('row');
+  const [rotationSpacer, setRotationSpacer] = useState<{ transform: [{ rotate: string }] }>()
 
   const languageSignLanguageMap : Record<string, string> = {
     'fr': 'fsl',
     'en': 'ase',
   };
+
+  useEffect(() => {
+	const updateLayout = () => {
+		const width = Dimensions.get('window').width;
+		setFlexDirection(width > 500 ? 'row' : 'column');
+		setRotationSpacer(width > 500 ? { transform: [{ rotate: '0deg' }] } : { transform: [{ rotate: '90deg' }] });
+	};
+	Dimensions.addEventListener('change', updateLayout);
+	updateLayout();
+	}, []);
+
 
   function handleFinishInput(input: string) {
     if (!input) {
@@ -30,11 +43,11 @@ const TextToPose = () => {
   }
 
   return (
-    <View style={styles.translate}>
+    <View style={[styles.translate, { flexDirection: flexDirection }]}>
       <View style={styles.box}>
         <InputBox onInputFinish={handleFinishInput} onLanguageCodeChange={handleLanguageCodeChange}/>
       </View>
-      <View style={styles.spacer}>
+      <View style={[styles.spacer, rotationSpacer ]}>
         <AntDesign name="swap" size={32} color="#DFDFDF"></AntDesign>
       </View>
       <View style={styles.box}>
@@ -47,29 +60,29 @@ const TextToPose = () => {
   );
 };
 
-let screen_width = Dimensions.get('window').width;
-let box_size = screen_width * 0.25;
+// let screen_width = Dimensions.get('window').width;
+// let box_size = screen_width * 0.25;
 
 const styles = StyleSheet.create({
   translate: {
     width: '100%',
-    height: box_size,
-    flexDirection: 'row',
+    height: 'auto',
     justifyContent: 'center',
     marginTop: 60,
   },
   box: {
-    width: box_size,
-    height: box_size,
+    width: 'auto',
+    height: 'auto',
     borderRadius: 10,
     overflow: 'hidden',
   },
   spacer: {
-    width: '5%',
-    height: box_size,
+    width: 'auto',
+    height: 'auto',
     justifyContent: 'center',
     alignItems: 'center',
   },
 });
 
 export default TextToPose;
+
